f31d5bd32b09ebd6f149a0fffa26a9d0c5a021f1
DESKTOP-7PGMHF1\losmos <iislamey@naver.com>
2022-08-11 18:05:41 +0900

    Go언어 Step04

--- go/src/Step_04_1차원배열/a4344_평균은넘겠지/main.go
+++ go/src/Step_04_1차원배열/a4344_평균은넘겠지/main.go
@@ -20,33 +20,36 @@ func main() {
 	scanner.Scan()
 	C, _ := strconv.Atoi(scanner.Text())
 
-	for i := 0; i < C; i++ {
+	for i := 0; i < C; i++ { // 테스트케이스 C 만큼 loop
 		scanner.Scan()
-		line := scanner.Text()
+		line := scanner.Text() // 한줄 읽어들여서
 
-		var sum int
-		var avg float64
-		scoreInfo := strings.Split(line, " ")
+		var sum int                           // 모든점수 sum한 값 저장할 변수
+		var avg float64                       // 평균 값 저장할 변수
+		scoreInfo := strings.Split(line, " ") // 읽어들인 정보 " "공백구분으로 split
 
-		for _, v := range scoreInfo {
+		for i, v := range scoreInfo { // sum 구함
+			if i == 0 {
+				continue
+			}
 			score, _ := strconv.Atoi(v)
 			sum += score
 		}
-		size := func() int {
+		size := func() int { // size 구하는 익명함수 ( var size int = stroconv.Atoi(scoreInfo[0]) 이렇게못씀. return이 두개라서 익명함수써봄)
 			size, _ := strconv.Atoi(scoreInfo[0])
 			return size
 		}()
 
-		avg = float64(sum) / float64(size)
+		avg = float64(sum) / float64(size) // 평균값
 
-		var overAvgCount int
+		var overAvgCount int // 평균을 넘는 사람 수 저장할 변수
 		for i, v := range scoreInfo {
 
 			if i == 0 {
 				continue
 			}
 
-			score, _ := strconv.ParseFloat(v, 64)
+			score, _ := strconv.ParseFloat(v, 64) // string to float (타입 변환)
 			if score > avg {
 				overAvgCount++
 			}
@@ -54,6 +57,6 @@ func main() {
 
 		result := float64(overAvgCount) / float64(size) * 100
 
-		fmt.Printf("%.2f", result)
+		fmt.Printf("%.3f%%\n", result)
 	}
 }
