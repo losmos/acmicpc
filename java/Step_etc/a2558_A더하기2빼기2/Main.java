13acfc38bfef3f54f1ecdfcf8455f67fdf4e49b6
losmos <iislamey@gmail.com>
2022-10-03 17:44:13 +0900

    [Java] 스트릭 유지용 문제

---
+++ java/Step_etc/a2558_A더하기2빼기2/Main.java
@@ -0,0 +1,26 @@
+/*
+ 	문제출처 : BACKJOON, https://www.acmicpc.net/problem/2558
+ 	문제번호 : 2558
+ 	알고리즘 : 수학, 구현, 사칙연산
+*/
+package Step_etc.a2558_A더하기2빼기2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()) + "");
+        br.close();
+        bw.flush();
+        bw.close();
+	}
+}
