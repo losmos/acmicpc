6337ba8d2c03b1215c01145906efca5ee4f9e22e
DESKTOP-7PGMHF1\losmos <iislamey@naver.com>
2022-10-07 17:37:01 +0900

    [Java] 폴더생성

---
+++ java/Step_16_동적계획법1/a24416_알고리즘수업_피보나치수1/Main.java
@@ -0,0 +1,19 @@
+﻿/*
+ 	문제출처 : BACKJOON, https://www.acmicpc.net/problem/24416
+ 	문제번호 : 24416
+ 	알고리즘 : 수학, 다이나믹 프로그래밍
+*/
+package Step_16_동적계획법1.a24416_알고리즘수업_피보나치수1;
+
+import java.io.IOException;
+
+
+/**
+ * [문제풀이 컨셉]
+ */
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+	}
+}
