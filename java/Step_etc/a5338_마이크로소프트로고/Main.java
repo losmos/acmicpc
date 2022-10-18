41491cceaf2a1207e45b0fa3dc2cc78ccde09cd4
losmos <iislamey@gmail.com>
2022-10-18 22:44:11 +0900

    [Java] 스트릭 유지용 문제

---
+++ java/Step_etc/a5338_마이크로소프트로고/Main.java
@@ -0,0 +1,28 @@
+/*
+ 	문제출처 : BACKJOON, https://www.acmicpc.net/problem/5338
+ 	문제번호 : 5338
+ 	알고리즘 : 구현
+*/
+package Step_etc.a5338_마이크로소프트로고;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("       _.-;;-._");
+        bw.write("\n");
+        bw.write("'-..-'|   ||   |");
+        bw.write("\n");
+        bw.write("'-..-'|_.-;;-._|");
+        bw.write("\n");
+        bw.write("'-..-'|   ||   |");
+        bw.write("\n");
+        bw.write("'-..-'|_.-''-._|");
+        bw.flush();
+        bw.close();
+	}
+}
