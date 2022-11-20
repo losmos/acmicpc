441949279500d21e3c83b294e9ce230feaeb17a2
losmos <iislamey@gmail.com>
2022-11-21 06:26:37 +0900

    [Java] 스트릭 유지용 문제

---
+++ java/Step_etc/a11365_밀비급일/Main.java
@@ -0,0 +1,38 @@
+﻿/*
+ 	문제출처 : BACKJOON, https://www.acmicpc.net/problem/11365
+ 	문제번호 : 11365
+ 	알고리즘 : 구현, 문자열
+*/
+package Step_etc.a11365_밀비급일;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringBuilder input = new StringBuilder();
+
+		while(true) {
+			input.append(br.readLine());
+
+			if(input.toString().equals("END")) {
+				break;
+			}
+
+			sb.append(input.reverse().append("\n"));
+			input.delete(0, sb.length());
+		}
+
+		br.close();
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
