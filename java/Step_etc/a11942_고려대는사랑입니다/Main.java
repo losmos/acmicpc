a8fe979dd6972f175b27bf0882a79afbebf3068b
losmos <iislamey@gmail.com>
2022-11-26 23:30:31 +0900

    [Java] 스트릭 유지용 문제

---
+++ java/Step_etc/a11942_고려대는사랑입니다/Main.java
@@ -0,0 +1,21 @@
+/*
+ 	문제출처 : BACKJOON, https://www.acmicpc.net/problem/11942
+ 	문제번호 : 11942
+ 	알고리즘 : 구현
+*/
+package Step_etc.a11942_고려대는사랑입니다;
+
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("고려대학교");
+        bw.flush();
+        bw.close();
+	}
+}
