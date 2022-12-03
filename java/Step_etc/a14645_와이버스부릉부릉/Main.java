ae8b7e5a03c7ed052e2ed54c5612d2c7db401cf9
losmos <iislamey@gmail.com>
2022-12-03 09:12:09 +0900

    [Java] 스트릭 유지용 문제

---
+++ java/Step_etc/a14645_와이버스부릉부릉/Main.java
@@ -0,0 +1,12 @@
+/*
+ 	문제출처 : BACKJOON, https://www.acmicpc.net/problem/14645
+ 	문제번호 : 14645
+ 	알고리즘 : 구현
+*/
+package Step_etc.a14645_와이버스부릉부릉;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("비와이");
+	}
+}
